62bf925e89b18088d5bb975ad8dcf9d302cdde73
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-07-01 22:46:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,4 +4,13 @@ class CreateArtists < ActiveRecord::Migration[5.2]
 
   def down
   end
+
+  def change
+  create_table :artists do |t|
+    t.string :name
+    t.string :genre
+    t.integer :age
+    t.string :hometown
+  end
+end
 end
